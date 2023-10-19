a6f2a70a1cc61a370632185d90ec465f6db97e8e
Ross Strickland <rkstrickland@gmail.com>
2023-10-18 20:20:35 -0600

    add tzdata

--- src/blockchain_explorer/setup.py
+++ src/blockchain_explorer/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(
-    name= "blockchain_explorer",
+    name="blockchain_explorer",
     install_requires=[
         "Django==4.2.6",
         "djangorestframework==3.14.0",
         "web3==6.11.0",
+        "tzdata==2023.3",
     ],
     packages=[
         "blockchain_explorer",
